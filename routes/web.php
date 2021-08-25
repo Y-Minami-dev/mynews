dfe53c14b01dba3b7cd548a245b0628b2735822b
Yusuke Honkawa <honyu.1993@outlook.jp>
2021-08-25 14:20:22 +0000

    PHP/Laravel 12_再提出

--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,11 @@ Route::group(['prefix'=>'admin','middleware'=>'auth'], function(){
     Route::post('profile/edit','Admin\ProfileController@update');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     
-    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
-    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
-    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
-    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
-    Route::post('news/edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news/create', 'Admin\NewsController@add');
+    Route::post('news/create', 'Admin\NewsController@create');
+    Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
 });
 
 
